5279d5cf3db551dfd3f7048e919d7564142c9353
piForever <pilarbenito98@hotmail.com>
2021-05-04 18:45:41 -0300

    changed to monsters game changed to monsters game

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let doorImage2 = document.getElementById("door2");
 let doorImage3 = document.getElementById("door3");
 let startButton = document.getElementById("start");
 ;
-let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg"
-let beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg"
-let spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg"
+let boo = "boo.png"
+let mike = "mike.png"
+let sullivan = "sullivan.png"
 
 let numClosedDoors= 4;
 let openDoor1 ;
@@ -16,8 +16,8 @@ let closedDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore
 let currentlyPlaying  = true ; 
 
 
-const isBot = (door) => {
-if (door.src === botDoorPath){
+const isBoo = (door) => {
+if (door.src === boo){
 return true;
 }else {
 return false ;
@@ -35,7 +35,7 @@ const playDoor = (door) => {
   numClosedDoors -- ;
   if (numClosedDoors === 0) {
     gameOver("win")
-  } else if (isBot(door)){
+  } else if (isBoo(door)){
 gameOver()
   }
 }
@@ -43,17 +43,17 @@ gameOver()
 const randomChoreDoorGenerator = () => {
 let choreDoor = Math.floor ( Math.random() * numClosedDoors);
 if (choreDoor === 0){
-openDoor1 = botDoorPath ;
-openDoor2 = beachDoorPath;
-openDoor3 = spaceDoorPath ;
+openDoor1 = boo ;
+openDoor2 = mike;
+openDoor3 = sullivan ;
 } else if (choreDoor === 1){
-openDoor1 = beachDoorPath;
-openDoor2 = botDoorPath;
-openDoor3 = spaceDoorPath;
+openDoor1 = mike;
+openDoor2 = boo;
+openDoor3 = sullivan;
 }else  {
-openDoor1 = spaceDoorPath ;
-openDoor2 = beachDoorPath;
-openDoor3 = botDoorPath ; 
+openDoor1 = sullivan ;
+openDoor2 = mike;
+openDoor3 = boo ; 
 }
 }
 
